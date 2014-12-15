febdfe0178ad415807d9329a895e4e5b1c154d4a
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2014-12-14 21:03:43 -0500

    Consider ELB headers when redirecting request

--- kaufman-beans/src/main/java/org/cyclopsgroup/kaufman/servlet/DomainRedirectionFilter.java
+++ kaufman-beans/src/main/java/org/cyclopsgroup/kaufman/servlet/DomainRedirectionFilter.java
@@ -16,11 +16,18 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Filter that redirects requests from secondary domains to the primary domain
+ * Filter that redirects requests to the primary domain if request is not from
+ * it. For example, it can be configured to redirect http://mysite.com to
+ * http://www.mysite.com, or from http://www.mysite.com to
+ * https://www.mysite.com.
  */
 public class DomainRedirectionFilter
     implements Filter
 {
+    private static final String HEADER_X_FORWARDED_PROTO = "X-Forwarded-Proto";
+
+    private static final String HEADER_X_FORWARDED_PORT = "X-Forwarded-Port";
+
     private static final Log LOG =
         LogFactory.getLog( DomainRedirectionFilter.class );
 
@@ -31,40 +38,76 @@ public class DomainRedirectionFilter
     {
     }
 
+    private static String buildUrl( String scheme, String hostName, int port )
+    {
+        StringBuilder url =
+            new StringBuilder( scheme.toLowerCase() ).append( "://" ).append( hostName );
+        if ( ( scheme.equalsIgnoreCase( "http" ) && port != 80 )
+            || ( scheme.equalsIgnoreCase( "https" ) && port != 443 ) )
+        {
+            url.append( ":" ).append( port );
+        }
+        return url.toString();
+    }
+
+    /**
+     * @inheritDoc
+     */
     @Override
     public void doFilter( ServletRequest request, ServletResponse response,
                           FilterChain chain )
         throws IOException, ServletException
     {
+        // Ignore requests from non-human
         if ( ServletUtils.isBot( request ) )
         {
             chain.doFilter( request, response );
             return;
         }
+
         HttpServletRequest req = (HttpServletRequest) request;
-        if ( !req.getMethod().equalsIgnoreCase( "get" ) )
+
+        // ignore non-GET reuqests
+        if ( req.getServerName().equals( "localhost" )
+            || !req.getMethod().equalsIgnoreCase( "get" ) )
         {
             chain.doFilter( request, response );
             return;
-        }
-        String requestedUrl = req.getRequestURL().toString();
-        String serverName = req.getServerName();
-        if ( requestedUrl.startsWith( baseUrl )
-            || serverName.equals( "localhost" ) )
-        {
-            chain.doFilter( request, response );
-            return;
-        }
-        String requestedBase = req.getScheme() + "://" + serverName;
-        if ( !requestedUrl.startsWith( requestedBase ) )
-        {
-            throw new IllegalStateException( "Requested URL = " + requestedUrl
-                + ", requestedBase = " + requestedBase
-                + ", something is wrong here!" );
         }
 
+        // If PROTO header is specified, the request scheme is what's specified
+        // in header instead of the value returned by getScheme() method.
+        String requestedScheme = req.getScheme();
+        if ( req.getHeader( HEADER_X_FORWARDED_PROTO ) != null )
+        {
+            requestedScheme = req.getHeader( HEADER_X_FORWARDED_PROTO );
+        }
+
+        // If PORT header is specified, the request scheme is what's specified
+        // in header instead of the value returned by getServerPort() method
+        int requestedPort = req.getServerPort();
+        if ( req.getHeader( HEADER_X_FORWARDED_PORT ) != null )
+        {
+            requestedPort =
+                Integer.parseInt( req.getHeader( HEADER_X_FORWARDED_PORT ) );
+        }
+        String requestedBase =
+            buildUrl( requestedScheme, req.getServerName(), requestedPort );
+
+        // If request is ready from primary domain, continue and return
+        if ( requestedBase.equalsIgnoreCase( baseUrl ) )
+        {
+            chain.doFilter( request, response );
+            return;
+        }
+
+        String requestedUrl = req.getRequestURL().toString();
         String redirectUrl =
-            baseUrl + StringUtils.removeStart( requestedUrl, requestedBase );
+            baseUrl
+                + StringUtils.removeStart( requestedUrl,
+                                           buildUrl( req.getScheme(),
+                                                     req.getServerName(),
+                                                     req.getServerPort() ) );
         LOG.info( "Redirecting request from " + requestedUrl
             + " to new location: " + redirectUrl );
         ( (HttpServletResponse) response ).sendRedirect( redirectUrl );
