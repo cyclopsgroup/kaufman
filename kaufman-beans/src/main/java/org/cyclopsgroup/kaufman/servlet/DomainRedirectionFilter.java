5e01804dc004be7651013e6e17a95522b2163312
Jiaqi Guo <jiaqi@cyclopsgroup.org>
2014-12-16 21:04:38 -0500

    Adding bingbot to blacklist

--- kaufman-beans/src/main/java/org/cyclopsgroup/kaufman/servlet/DomainRedirectionFilter.java
+++ kaufman-beans/src/main/java/org/cyclopsgroup/kaufman/servlet/DomainRedirectionFilter.java
@@ -9,9 +9,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -28,11 +28,93 @@ public class DomainRedirectionFilter
 
     private static final String HEADER_X_FORWARDED_PORT = "X-Forwarded-Port";
 
+    private static class ForwardedHttpRequest
+        extends HttpServletRequestWrapper
+    {
+        private final int serverPort;
+
+        private final String scheme;
+
+        private final boolean forwarded;
+
+        protected ForwardedHttpRequest( HttpServletRequest request )
+        {
+            super( request );
+
+            boolean changed = false;
+            if ( request.getHeader( HEADER_X_FORWARDED_PORT ) != null )
+            {
+                this.serverPort =
+                    Integer.parseInt( request.getHeader( HEADER_X_FORWARDED_PORT ) );
+                changed = true;
+            }
+            else
+            {
+                this.serverPort = super.getServerPort();
+            }
+
+            if ( request.getHeader( HEADER_X_FORWARDED_PROTO ) != null )
+            {
+                this.scheme = request.getHeader( HEADER_X_FORWARDED_PROTO );
+                changed = true;
+            }
+            else
+            {
+                this.scheme = super.getScheme();
+            }
+            this.forwarded = changed;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public StringBuffer getRequestURL()
+        {
+            if ( !forwarded )
+            {
+                return super.getRequestURL();
+            }
+            String originalBase =
+                buildUrl( super.getScheme(), super.getServerName(),
+                          super.getServerPort() );
+            String url = super.getRequestURL().toString();
+            if ( url.startsWith( originalBase ) )
+            {
+                String newBase =
+                    buildUrl( scheme, super.getServerName(), serverPort );
+                return new StringBuffer( newBase ).append( url.substring( originalBase.length() ) );
+            }
+            return super.getRequestURL();
+        }
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public String getScheme()
+        {
+            return scheme;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public int getServerPort()
+        {
+            return serverPort;
+        }
+    }
+
     private static final Log LOG =
         LogFactory.getLog( DomainRedirectionFilter.class );
 
     private String baseUrl;
 
+    /**
+     * @inheritDoc
+     */
     @Override
     public void destroy()
     {
@@ -65,49 +147,30 @@ public class DomainRedirectionFilter
             return;
         }
 
-        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletRequest req =
+            new ForwardedHttpRequest( (HttpServletRequest) request );
 
         // ignore non-GET reuqests
         if ( req.getServerName().equals( "localhost" )
             || !req.getMethod().equalsIgnoreCase( "get" ) )
         {
-            chain.doFilter( request, response );
+            chain.doFilter( req, response );
             return;
         }
 
-        // If PROTO header is specified, the request scheme is what's specified
-        // in header instead of the value returned by getScheme() method.
-        String requestedScheme = req.getScheme();
-        if ( req.getHeader( HEADER_X_FORWARDED_PROTO ) != null )
-        {
-            requestedScheme = req.getHeader( HEADER_X_FORWARDED_PROTO );
-        }
-
-        // If PORT header is specified, the request scheme is what's specified
-        // in header instead of the value returned by getServerPort() method
-        int requestedPort = req.getServerPort();
-        if ( req.getHeader( HEADER_X_FORWARDED_PORT ) != null )
-        {
-            requestedPort =
-                Integer.parseInt( req.getHeader( HEADER_X_FORWARDED_PORT ) );
-        }
-        String requestedBase =
-            buildUrl( requestedScheme, req.getServerName(), requestedPort );
+        String requestBase =
+            buildUrl( req.getScheme(), req.getServerName(), req.getServerPort() );
 
         // If request is ready from primary domain, continue and return
-        if ( requestedBase.equalsIgnoreCase( baseUrl ) )
+        if ( requestBase.equalsIgnoreCase( baseUrl ) )
         {
-            chain.doFilter( request, response );
+            chain.doFilter( req, response );
             return;
         }
 
         String requestedUrl = req.getRequestURL().toString();
         String redirectUrl =
-            baseUrl
-                + StringUtils.removeStart( requestedUrl,
-                                           buildUrl( req.getScheme(),
-                                                     req.getServerName(),
-                                                     req.getServerPort() ) );
+            baseUrl + requestedUrl.substring( requestBase.length() );
         LOG.info( "Redirecting request from " + requestedUrl
             + " to new location: " + redirectUrl );
         ( (HttpServletResponse) response ).sendRedirect( redirectUrl );
--- kaufman-beans/src/main/java/org/cyclopsgroup/kaufman/servlet/ServletUtils.java
+++ kaufman-beans/src/main/java/org/cyclopsgroup/kaufman/servlet/ServletUtils.java
@@ -14,6 +14,7 @@ public class ServletUtils
         Collections.unmodifiableSet( new HashSet<String>(
                                                           Arrays.asList( "googlebot",
                                                                          "msnbot",
+                                                                         "bingbot",
                                                                          "baiduspider",
                                                                          "elb-healthchecker",
                                                                          "zmeu",
